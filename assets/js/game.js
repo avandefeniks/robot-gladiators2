65fd32b7e572f80f7f11a3824c8ae4d595024a47
avandefeniks <angel.vandefeniks@gmail.com>
2023-09-29 15:38:29 -0700

    changed shop switch statement to accept integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -157,22 +157,21 @@ function shop() {
     // console.log("entered the shop");
     // ask player what they'd like to do
     let shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-    );
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 " +
+        "to 'REFILL HEALTH', 2 to 'UPGRADE ATTACK', or 3 to 'LEAVE.'");
+
+        shopOptionPrompt = parseInt(shopOptionPrompt);
 
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             // do nothing so function will end
             break;
