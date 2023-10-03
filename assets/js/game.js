ac3f089889379d2c3fa7a6dbf0669447592051c1
avandefeniks <angel.vandefeniks@gmail.com>
2023-10-03 14:30:08 -0700

    added high score using local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -162,10 +162,29 @@ function startGame() {
 function endGame() {
     // if player is still alive, player wins
     if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        // window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("The game has ended. Let's see how you did!");
     }
     else {
         window.alert("You've lost your robot in battle.");
+    }
+
+    // check local storage for high score, if it's not there, use 0
+    let highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    // if player has more money than the high score, player has new high score!
+    // debugger
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they want to play again
@@ -178,7 +197,7 @@ function endGame() {
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-    //window.alert("The game has ended. Let's see how you did!");
+    
 };
 
 function shop() {
